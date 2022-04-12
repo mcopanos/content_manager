730ead8124791b1252c981092da2e3b38bfa929f
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2022-04-12 12:32:49 -0500

    edit contact complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './componets/layout/Header';
 import ContactsFun from './componets/contacts/ContactsFun';
 import About from './componets/pages/About';
