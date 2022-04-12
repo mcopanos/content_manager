8084cc2d064d858d39fb368279b5269ccb1dfebe
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2022-04-12 17:50:41 -0500

    more cleanup

--- src/componets/contacts/AddContact.js
+++ src/componets/contacts/AddContact.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Consumer } from '../../context';
-import { v4 as uuidv4} from 'uuid';
 import TextInputGroup from '../layout/TextInputGroup';
 import axios from 'axios';
 
